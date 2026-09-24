8f06b109e2ffdbb67773d605f8cc1ffe0463e7db
debrizt <dbrizuela99@gmail.com>
2026-09-24 08:52:55 -0500

    Fetch jobs from multiple companies

--- src/jobrank/fetch.py
+++ src/jobrank/fetch.py
@@ -1,22 +1,40 @@
 import httpx
 
-# board variable that will be where it is pulling from 
-BOARD = "stripe"
-URL = f"https://boards-api.greenhouse.io/v1/boards/{BOARD}/jobs"
+# companies variable that will be where it is pulling from 
+COMPANIES = ["stripe", "airbnb", "discord", "figma", "databricks", "cloudflare", "robinhood", "dropbox", "pinterest", "reddit", "lyft", "gitlab"]
+
+#jobs list matchign with company
+all_jobs = []
+
+for company in COMPANIES:
+
+    url = f"https://boards-api.greenhouse.io/v1/boards/{company}/jobs"
 
 
-# get the JSON from the URL
-response = httpx.get(URL)
+    try: 
+        # get the JSON from the URL
+        response = httpx.get(url)
+        
+        #throw an exception if it did not work
+        response.raise_for_status()
 
-#if error will let us know here
-response.raise_for_status()
+        #get the list of jobs (each a dict entry per job)
+        jobs = response.json()["jobs"]
 
-#get the list from jobs
-jobs = response.json()["jobs"] 
+        for job in jobs:
+            #add the company value into it
+            job["company"] = company
 
+        print(f"{company} { len(jobs)}")
 
-print(f"{len(jobs)} jobs at {BOARD}")
-for job in jobs[:10]:
-    print(f"- {job['title']} ({job['location']['name']})")
+        #add it to the all jobs
+        all_jobs.extend(jobs)
 
+    except httpx.HTTPStatusError:
+        print(f"Skipping {company}")
+
+#print the total count
+print(f"All jobs: {len(all_jobs)}")
+
+    
 
